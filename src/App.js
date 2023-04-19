57a26b8bb7601f35503197d061190d33f4f12dfd
Dysio <td.dyszewski@gmail.com>
2023-04-19 21:33:25 +0200

    lesson 4 Creating Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import PropTypes from "prop-types";
 import './App.css';
-import pokemon from './pokemon.json'
+import pokemon from './pokemon.json';
+
+const PokemonRow = ({ pokemon }) => (
+  <tr>
+    <td>{ pokemon.name.english }</td>
+    <td>{ pokemon.type.join(", ") }</td>
+  </tr>
+);
+
+PokemonRow.propTypes = {
+  pokemon : PropTypes.shape({
+    name : PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  })
+};
 
 function App() {
   return (
@@ -20,13 +37,8 @@ function App() {
         </thead>
         <tbody>
           { pokemon.slice(0,20).map(pokemon => (
-            <tr
-              key={[pokemon.id, pokemon.name.english]}
-            >
-              <td>{ pokemon.name.english }</td>
-              <td>{ pokemon.type.join(", ") }</td>
-            </tr>
-          )) }
+            <PokemonRow pokemon={pokemon} key={pokemon.id}/>
+        )) }
         </tbody>
       </table>
     </div>
